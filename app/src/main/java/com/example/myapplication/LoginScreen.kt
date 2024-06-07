2c81ffa5623d02c2a1c89be1e978276d1a174a32
DanteCS25 <cs.dantestrijdom@gmail.com>
2024-06-08 00:17:35 +0200

    Final Design

--- app/src/main/java/com/example/myapplication/LoginScreen.kt
+++ app/src/main/java/com/example/myapplication/LoginScreen.kt
@@ -43,7 +43,8 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             contentScale = ContentScale.FillBounds
         )
 
-        // Overlay Box
+        // TODO: Overlay Box
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -60,51 +61,57 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             ) {
                 Spacer(modifier = Modifier.height(30.dp)) // Add some space at the top
 
-                // Name
-                Text(
-                    text = "LOGIN",
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 56.sp // Set the text size
-                )
 
-                Spacer(modifier = Modifier.height(16.dp)) // Space between items
+// TODO: Login Text
 
-                // Label for Email input
+               Text(
+                   text = "LOGIN",
+                   color = Color.White,
+                   fontWeight = FontWeight.Bold,
+                   fontSize = 56.sp
+               )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+// TODO: Label for Email input
+
                 Text(
                     text = "Your email",
                     color = Color.White,
-                    fontSize = 16.sp, // Set the text size
-                    fontWeight = FontWeight.W400, // Set the font weight to 400
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W400,
                     modifier = Modifier
-                        .fillMaxWidth() // Make sure it takes the full width
-                        .padding(horizontal = 40.dp) // Align to the left by adding padding to both sides
+                        .fillMaxWidth()
+                        .padding(horizontal = 40.dp)
                 )
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                // Email input
+// TODO: Email input
+
                 Box(
                     modifier = Modifier
                         .width(361.dp)
                         .height(51.dp)
-                        .padding(horizontal = 16.dp) // Keep the original padding
-                        .border(1.dp, Color.White, RoundedCornerShape(50.dp)) // Add white border with rounded corners
+                        .padding(horizontal = 16.dp)
+                        .border(1.dp, Color.White, RoundedCornerShape(50.dp))
                 ) {
                     BasicTextField(
-                        value = "eg. martian@space.com", // Value of the text field
-                        onValueChange = {}, // Change listener for the text field
-                        textStyle = androidx.compose.ui.text.TextStyle(color = Color.White), // Text style for the text field
-                        singleLine = true, // Ensure only one line is shown
+                        value = "eg. martian@space.com",
+                        onValueChange = {},
+                        textStyle = androidx.compose.ui.text.TextStyle(color = Color.White),
+                        singleLine = true,
                         modifier = Modifier
-                            .fillMaxSize() // Ensure the text field fills the entire Box
-                            .padding(start = 20.dp, top = 16.dp, end = 20.dp, bottom = 16.dp) // Add padding to align the text
+                            .fillMaxSize()
+                            .padding(start = 20.dp, top = 16.dp, end = 20.dp, bottom = 16.dp)
                     )
                 }
 
-                Spacer(modifier = Modifier.height(40.dp)) // Space between items
+                Spacer(modifier = Modifier.height(40.dp))
 
-                // Label for Password input
+
+// TODO: Label for Password input
+
                 Text(
                     text = "Your password",
                     color = Color.White,
@@ -116,7 +123,8 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                 )
                 Spacer(modifier = Modifier.height(10.dp))
 
-                // Password input
+// TODO: Password input
+
                 Box(
                     modifier = Modifier
                         .width(361.dp)
@@ -136,14 +144,16 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                 }
                 Spacer(modifier = Modifier.height(16.dp)) // Space between password field and texts
 
+// TODO: Remember Me
+
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween // Arrange children horizontally with space between them
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Row(
-                        verticalAlignment = Alignment.CenterVertically // Center items vertically within the row
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         Checkbox(
                             checked = false,
@@ -152,39 +162,44 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                         Text(
                             text = "Remember Me",
                             color = Color.White,
-                            fontSize = 12.sp // Set the text size
+                            fontSize = 12.sp
                         )
                     }
                     Text(
                         text = "Forgot Password",
                         color = Color(0xFFF2994A),
-                        fontSize = 12.sp, // Set the text size
-                        modifier = Modifier.align(Alignment.CenterVertically) // Ensure vertical alignment
+                        fontSize = 12.sp,
+                        modifier = Modifier.align(Alignment.CenterVertically)
                     )
                 }
 
-                // Button
-                Spacer(modifier = Modifier.height(40.dp)) // Space between items
+// TODO: Button
+
+                Spacer(modifier = Modifier.height(40.dp))
+
                 Button(
                     onClick = { /*TODO*/ },
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFF7920C2) // Replace with your desired color
+                        containerColor = Color(0xFF7920C2)
                     ),
                     modifier = Modifier
-                        .width(311.dp) // Fixed width
+                        .width(311.dp)
                         .height(55.dp)
                         .clip(RoundedCornerShape(50.dp))
-                        .padding(horizontal = 16.dp) // Keep the original padding
+                        .padding(horizontal = 16.dp)
                 ) {
                     Text(
                         text = "Login",
                         style = androidx.compose.ui.text.TextStyle(color = Color.White),
                         fontWeight = FontWeight.Bold,
-                        fontSize = 16.sp // Set the text size
+                        fontSize = 16.sp
                     )
                 }
 
-                Spacer(modifier = Modifier.height(40.dp)) // Space between items
+                Spacer(modifier = Modifier.height(40.dp))
+
+// TODO: Already have an account?
+
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -196,37 +211,38 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                         Text(
                             text = "Already have an account? ",
                             color = Color.White,
-                            fontSize = 16.sp // Set the text size
+                            fontSize = 16.sp
                         )
                     }
                     Text(
                         text = "Sign Up",
                         color = Color(0xFFF2994A),
-                        fontSize = 16.sp // Set the text size
+                        fontSize = 16.sp
                     )
                 }
             }
         }
 
-        // Circular image with overlapping
+        // TODO: Circular image with overlapping
+
         Box(
             modifier = Modifier
-                .align(Alignment.TopCenter) // Align at the top center of the Box
-                .offset(y = 160.dp) // Adjust this value to position the circle shape over the edge of the box
-                .size(113.dp) // Set width and height to 113px
-                .clip(CircleShape) // Clip the Box to a circle shape
-                .background(Color(0xFFF2994A)) // Background color of the circle
-            ,
-            contentAlignment = Alignment.Center // Center the content within the Box
+                .align(Alignment.TopCenter)
+                .offset(y = 160.dp)
+                .size(113.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFF2994A)),
+            contentAlignment = Alignment.Center
         ) {
             Icon(
-                painter = painterResource(id = icon), // Replace with your icon resource
+                painter = painterResource(id = icon),
                 contentDescription = null,
                 modifier = Modifier
                     .size(78.dp),
-                tint = Color.White // Tint the icon color if needed
+                tint = Color.White
             )
         }
+
     }
 }
 
